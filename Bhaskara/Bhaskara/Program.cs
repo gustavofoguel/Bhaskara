3fcf65af07568b7f8e2ccb38549ad3d28f7645a7
Gustavo Foguel <gustavofoguel123@outlook.com>
2023-01-06 13:51:46 -0300

    Initial commit

---
+++ Bhaskara/Bhaskara/Program.cs
@@ -0,0 +1,41 @@
+﻿ using System;
+
+namespace Bhaskara
+{
+    using System;
+
+    namespace BhaskaraFormula
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Insira os valores de a, b e c para a equação de segundo grau (ax^2 + bx + c = 0):");
+                Console.Write("a = ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b = ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("c = ");
+                double c = double.Parse(Console.ReadLine());
+
+                double delta = b * b - 4 * a * c;
+
+                if (delta < 0)
+                {
+                    Console.WriteLine("A equação não possui raízes reais.");
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    Console.WriteLine("A equação possui apenas uma raiz real: x = " + x);
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    Console.WriteLine("A equação possui duas raízes reais: x1 = " + x1 + ", x2 = " + x2);
+                }
+            }
+        }
+    }
+}
